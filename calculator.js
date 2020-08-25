cfeb5e57d59f06a09b67b142a23efab5815f0c5a
Alexander <alex@gmail.com>
2020-08-25 09:46:55 +0300

    load project

---
+++ calculator.js
@@ -0,0 +1,37 @@
+'use strict'
+
+//Первый расчет
+function showFirstResult(){
+	alert('Показать первый результат')
+}
+showFirstResult();
+
+
+function calc1(a,b){
+	return(a+b)
+}
+console.log(calc1(3,5))
+
+
+//Второй расчет
+function showTwiceResult(){
+	alert('Показать второй результат')
+}
+showTwiceResult();
+
+function calc2(a,b,c){
+	return(a+b-c)
+}
+console.log(calc2(10,5,3))
+console.log(calc2(5,5,10))
+
+//Третий расчет
+function showThirdResult(){
+	alert('Показать третий результат')
+}
+showThirdResult();
+
+function calc3(a,b,c,d){
+	return(a+b*c/d)
+}
+console.log(calc3(15,15,2,2))
